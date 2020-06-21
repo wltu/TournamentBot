39efd2d6668ac6a0af5f15b83079e319b1162bf5
Willy Tu <tu.willy.97@gmail.com>
2020-06-21 14:46:45 -0700

    Finish basic music

--- cogs/common.py
+++ cogs/common.py
@@ -9,10 +9,7 @@ from discord.ext import commands
 class Common(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.current_song = None
-        self.song_queue = []
-        self.channel_queue = []
-        self.voice_channel = None
+        self.playing_music = {}
 
     def user_channel(self, ctx):
         if ctx.message.author.voice is None:
@@ -20,8 +17,8 @@ class Common(commands.Cog):
 
         return ctx.message.author.voice.channel, True
 
-    def clean_songs(self):
-        dir = "./play"
+    def clean_songs(self, guild_id):
+        dir = "./play/" + str(guild_id)
         filelist = [f for f in os.listdir(dir) if f.endswith(".mp3")]
         for f in filelist:
             os.remove(os.path.join(dir, f))
@@ -48,31 +45,7 @@ class Common(commands.Cog):
 
         return voice
 
-    async def play_next(self):
-        self.song_queue = self.song_queue[1:]
-        self.channel_queue = self.channel_queue[1:]
-
-        if len(self.song_queue) > 0:
-            print("Play next song!")
-            if not self.voice_channel:
-                self.voice_channel = await self.channel_queue[0].connect()
-            elif self.voice_channel.channel != self.channel_queue[0]:
-                await self.voice_channel.move_to(self.channel_queue[0])
-
-            self.play_audio(self.voice_channel)
-        else:
-            await self.voice_channel.disconnect()
-            self.voice_channel = None
-            # self.clean_songs()
-
-    def after_audio(self, error):
-        try:
-            func = asyncio.run_coroutine_threadsafe(self.play_next(), self.bot.loop)
-            func.result()
-        except Exception as e:
-            print(e)
-
-    def play_audio(self, voice, song=None):
+    def play_audio(self, voice, song):
         if os.name != "nt" and not discord.opus.is_loaded():
             try:
                 # full extension for alpine docker image
@@ -82,15 +55,8 @@ class Common(commands.Cog):
 
         if voice.is_playing():
             voice.stop()
-
-        self.voice_channel = voice
-
-        if not song:
-            audio_source = discord.FFmpegPCMAudio(self.song_queue[0])
-            voice.play(audio_source, after=self.after_audio)
-        else:
-            audio_source = discord.FFmpegPCMAudio(song)
-            voice.play(audio_source, after=None)
+        audio_source = discord.FFmpegPCMAudio(song)
+        voice.play(audio_source)
 
     @commands.command(name="test")
     async def test(self, ctx, arg):
@@ -100,9 +66,14 @@ class Common(commands.Cog):
     @commands.command(name="play")
     async def play(self, ctx, url):
         """ Play Music from YouTube """
-        channel = ctx.message.author.voice.channel
-        voice = get(self.bot.voice_clients, guild=ctx.guild)
+        if not ctx.message.author.voice:
+            await ctx.send("You are not in a channel :angry:")
+            return
 
+        channel = ctx.message.author.voice.channel
+
+        voice = get(self.bot.voice_clients, guild=ctx.guild)
+        guild_id = str(ctx.guild.id)
         ydl_opts = {
             "format": "bestaudio/best",
             "postprocessors": [
@@ -112,41 +83,31 @@ class Common(commands.Cog):
                     "preferredquality": "192",
                 }
             ],
-            "outtmpl": "./play/%(title)s.%(ext)s",
+            "outtmpl": "./play/" + guild_id + "/%(title)s.%(ext)s",
             "forcefilename": True,
         }
 
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            info = ydl.extract_info(url, download=False)
-            filename = ydl.prepare_filename(info)
-            filename = filename[:-5]
-
-            if filename + ".mp3" not in self.song_queue:
-                print("Downloading audio!")
-                ydl.download([url])
-
-            self.song_queue.append(filename + ".mp3")
-            self.channel_queue.append(channel)
-
             if not voice or not voice.is_playing():
+                print("Downloading audio!")
+
+                info = ydl.extract_info(url, download=True)
+                filename = ydl.prepare_filename(info)
+                filename = filename[:-5]
+
                 if not voice or not voice.is_connected():
                     if not voice:
                         voice = await channel.connect()
                     else:
                         await voice.move_to(channel)
-
+                
                 await ctx.send(
-                    "Playing `" + filename[5:] + "` in `" + str(channel) + "`"
+                    "Playing `" + filename[8 + len(guild_id):] + "` in `" + str(channel) + "`"
                 )
-                self.play_audio(voice)
+                self.playing_music[ctx.guild.id] = True 
+                self.play_audio(voice, filename + ".mp3")
             else:
-                await ctx.send(
-                    "Putting `"
-                    + filename[5:]
-                    + "` in `"
-                    + str(channel)
-                    + "` in the queue."
-                )
+                await ctx.send("Music is being played in " + str(voice.channel) + "!")
 
     @commands.command(name="join")
     async def join(self, ctx):
@@ -174,6 +135,8 @@ class Common(commands.Cog):
         voice = get(self.bot.voice_clients, guild=ctx.guild)
 
         if voice is not None:
+            self.playing_music[ctx.guild.id] = False
+            self.clean_songs(ctx.guild.id)
             await voice.disconnect()
         else:
             await ctx.send("I am currently not in a voice chat! :cry:")
